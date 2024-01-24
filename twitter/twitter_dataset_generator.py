1841a8dea7233d3f3c99fd49d8ad66576519574b
SeMinLim <seminl1@ics.uci.edu>
2024-01-24 08:40:22 +0000

    modified twitter dataset generator python code

--- twitter/twitter_dataset_generator.py
+++ twitter/twitter_dataset_generator.py
@@ -1,8 +1,10 @@
 import torch
 import torch_geometric
+import torch_geometric.transforms as T
 import numpy as np
 import random
 from torch_geometric.data import Data
+from torch_geometric.typing import SparseTensor
 
 
 # Function for reading dataset file
@@ -28,10 +30,10 @@ def read_file_until_tab_newline(file):
 
 
 # PyTorch availability and GPU checking
-print('PyTorch Availability:', torch.cuda.is_available())
-print('The Number of GPUs:', torch.cuda.device_count())
-print('The Current GPU Index:', torch.cuda.current_device())
-print('The Current GPU Name:', torch.cuda.get_device_name(0))
+print('PyTorch Availability:', torch.cuda.is_available(), flush=True)
+print('The Number of GPUs:', torch.cuda.device_count(), flush=True)
+print('The Current GPU Index:', torch.cuda.current_device(), flush=True)
+print('The Current GPU Name:', torch.cuda.get_device_name(0), flush=True)
 
 
 # Read the file and store the dataset
@@ -52,16 +54,69 @@ file.close()
 
 ## Edges
 # Check the list length and node's max and min
-print('The Length of Edge List:', len(edges))
-print('Max of Nodes:', max(edges))
-print('Min of Nodes:', min(edges))
+print('The Length of Edge List:', len(edges), flush=True)
+print('Max of Nodes:', max(edges), flush=True)
+print('Min of Nodes:', min(edges), flush=True)
 
 # Change edge_index list to numpy array first
 edges = np.array(edges)
-print('The Shape of Edge List:', edges.shape)
+print('1D Edge Array:', edges.shape, flush=True)
+
+# Get the nodes
+nodes = np.unique(edges)
+nodes.sort()
+print('1D Node Array', nodes.shape, flush=True)
+
 # Reshape edge_index numpy array to 2-dimensional
 edges = edges.reshape(-1, 2)
-print('The Shape of The Original Edges:', edges.shape)
+print('2D Directed Edge Array (Dense):', edges.shape, flush=True)
+
+# Generate the modified node
+m_nodes=[]
+for i in range(41652230):
+    m_nodes.append(i)
+
+m_nodes = np.array(m_nodes)
+nodes = np.stack((nodes, m_nodes), axis=1)
+
+# Sort edge_index based on column 1
+tmp_nodes = edges[:, 0]
+edges = edges[tmp_nodes.argsort()]
+print('Sorting 2D Directed Edge Array by Column 1 Done!', flush=True)
+
+# Change the original nodes to modified nodes (Phase 1)
+num_edges = 1468365182
+num_nodes = 41652230
+pointer = 0
+for i in range(num_edges):
+    for j in range(pointer, num_nodes):
+        if (edges[i][0] == nodes[j][0]):
+            edges[i][0] = nodes[j][1]
+            pointer = j
+            break
+print('Modifying Node Index Column 1 Done!', flush=True)
+
+# Sort edge_index based on column 2
+tmp_nodes = edges[:, 1]
+edges = edges[tmp_nodes.argsort()]
+print('Sorting 2D Directed Edge Array by Column 2 Done!', flush=True)
+
+# Change the original nodes to modified nodes (Phase 2)
+pointer = 0
+for i in range(num_edges):
+    for j in range(pointer, num_nodes):
+        if (edges[i][1] == nodes[j][0]):
+            edges[i][1] = nodes[j][1]
+            pointer = j
+            break
+print('Modifying Node Index Column 2 Done!', flush=True)
+
+# Sort modified edge_index based on column 1 again
+tmp_nodes = edges[:, 0]
+edges = edges[tmp_nodes.argsort()]
+print('Sorting 2D Directed Edge Array by Column 1 Again Done!', flush=True)
+print(edges[0], flush=True)
+print(edges[1468365181], flush=True)
 
 # Generate the opposite directional edges first
 opposite = []
@@ -71,22 +126,31 @@ for i in range(1468365182):
     opposite.append(edge[0])
 
 opposite = np.array(opposite)
-print('The Shape of Primary Opposite Edges List:', opposite.shape)
 opposite = opposite.reshape(-1, 2)
-print('The Shape of Primary Opposite Edges:', opposite.shape)
+print('2D Opposite Directed Edges Array (Dense):', opposite.shape, flush=True)
 
 # Merge the original and opposite edges
 edges = np.append(edges, opposite, axis=0)
-print('The Shape of Primary Bidirectional Edges:', edges.shape)
+print('2D Undirected Edges Array (Dense):', edges.shape)
 
 # Delete duplicates of the edges
 edges = np.unique(edges, axis=0)
-print('The Shape of Final Bidirectional Edges:', edges.shape)
+print('Final 2D Undirected Edges Array (Dense):', edges.shape, flush=True)
 
 # Make edge_index as tensor for using it on PyTorch Geometric
 # dtype should be torch.int64
 edges = torch.tensor(edges, dtype=torch.int64)
-print('Final Edges:', edges.shape)
+edges = edges.t().contiguous()
+print('2D Undirected Edges Tensor (Dense):', edges.shape, flush=True)
+
+# Change edge_index to sparse
+edges = SparseTensor(
+        row=edges[1],
+        col=edges[0],
+        sparse_sizes=(41652230, 41652230),
+        is_sorted=True,
+        trust_data=True,)
+print(edges, flush=True)
 
 
 ## Node Feature Matrix
@@ -103,19 +167,17 @@ for i in range(41652230):
     x.extend(tmp)
     tmp.clear()
 
-print('Node Feature List:', len(x))
-
 # Change node feature matrix(list) to numpy array first
 x = np.array(x)
-print('The Shape of Node Feature List:', x.shape)
+print('1D Node Feature Array:', x.shape, flush=True)
 # Reshape node feature matrix(numpy array) to 2-dimensional
 x = x.reshape(-1, 16)
-print('The Shape of Node Feature Matrix:', x.shape)
+print('2D Node Feature Array:', x.shape, flush=True)
 
 # Make node feature matrix as tensor for using it on PyTorch Geometric
 # dtype should be torch.float32
 x = torch.tensor(x, dtype=torch.float32)
-print('Node Feature Matrix:', x.shape)
+print('2D Node Feature Tensor:', x.shape, flush=True)
 
 
 ## Ground-Truth Labels
@@ -125,19 +187,16 @@ for i in range(41652230):
     r = random.randrange(0, 16)
     y.append(r)
 
-print('The Shape of Ground-Truth Labels:', len(y))
-
 # Make ground-truth lables as tensor for using it on PyTorch Geometric
 # dtype should be torch.int64
 y = torch.tensor(y, dtype=torch.int64)
-print('Ground-Truth Labels:', y.shape)
+print('1D Ground-Truth Label Tensor:', y.shape, flush=True)
 
 
 ## Save the dataset as .pt
 # Make node feature matrix, edge index, ground-truth labels as PyTorch Dataset
-data = Data(x=x, edge_index=edges.t().contiguous(), y=y)
-print('Final PyTorch Dataset:', data)
+data = Data(x=x, y=y, adj_t=edges)
+print(data, flush=True)
 
 # Save the data
 torch.save(data, "/mnt/ephemeral/gnn/dataset/Twitter/twitter.pt")
-
